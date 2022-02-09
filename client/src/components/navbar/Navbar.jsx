2e70af683d4ecff1a843bc84f8555f65c3162383
jydeveloper <lukaryu@naver.com>
2022-02-09 18:40:34 +0900

    알림 메세지 띄워주기

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -6,6 +6,7 @@ import Settings from '../../img/settings.svg';
 
 const Navbar = ({ socket }) => {
 	const [notifications, setNotifications] = useState([]);
+	const [open, setOpen] = useState(false);
 
 	useEffect(() => {
 		socket.on('getNotification', (data) => {
@@ -14,24 +15,40 @@ const Navbar = ({ socket }) => {
 	}, [socket]);
 
 	console.log(notifications);
+	console.log(open);
+
+	const displayNotification = ({ senderName, type }) => {
+		let action;
+
+		if (type === 1) {
+			action = 'liked';
+		} else if (type === 2) {
+			action = 'commented';
+		} else if (type === 3) {
+			action = 'shared';
+		}
+
+		return <span className="notification">{`${senderName} ${action} your post`}</span>;
+	};
 
 	return (
 		<div className="navbar">
 			<span className="logo">React Socket io</span>
 			<div className="icons">
-				<div className="icon">
+				<div className="icon" onClick={() => setOpen(!open)}>
 					<img src={Notification} className="iconImg" alt="" />
 					<div className="counter">2</div>
 				</div>
-				<div className="icon">
+				<div className="icon" onClick={() => setOpen(!open)}>
 					<img src={Message} className="iconImg" alt="" />
 					<div className="counter">2</div>
 				</div>
-				<div className="icon">
+				<div className="icon" onClick={() => setOpen(!open)}>
 					<img src={Settings} className="iconImg" alt="" />
 					<div className="counter">2</div>
 				</div>
 			</div>
+			{open && <div className="notifications">{notifications.map((n) => displayNotification(n))}</div>}
 		</div>
 	);
 };

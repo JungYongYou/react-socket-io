b8a1bfaf4dfd755bd0102b5a9f8aaef59ccc26c8
jydeveloper <lukaryu@naver.com>
2022-02-09 16:20:42 +0900

    디자인 적용

---
+++ client/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import './App.css';
+import Navbar from './components/navbar/Navbar';
+import Card from './components/card/Card';
+import { posts } from './data';
+
+function App() {
+	const [username, setUsername] = useState('');
+	const [user, setUser] = useState('');
+
+	return (
+		<div className="container">
+			{user ? (
+				<>
+					<Navbar />
+					{posts.map((post) => (
+						<Card key={post.id} post={post} />
+					))}
+					<span className="username">{user}</span>
+				</>
+			) : (
+				<div className="login">
+					<input type="text" placeholder="username" onChange={(e) => setUsername(e.target.value)} />
+					<button onClick={() => setUser(username)}>Login</button>
+				</div>
+			)}
+		</div>
+	);
+}
+
+export default App;
---
+++ client/src/data.js
@@ -0,0 +1,17 @@
+export const posts = [
+	{
+		id: 1,
+		userName: 'yu_jung_kim',
+		fullName: 'Yu Jung Kim',
+		userImg: 'https://thumb.mt.co.kr/06/2020/11/2020112410565623096_1.jpg/dims/optimize/',
+		postImg: 'https://newsimg.sedaily.com/2020/04/08/1Z1E8KLUL3_1.jpg',
+	},
+	{
+		id: 2,
+		userName: 'da_mi_kim',
+		fullName: 'Da Mi Kim',
+		userImg: 'https://dimg.donga.com/wps/NEWS/IMAGE/2019/01/10/93645765.3.jpg',
+		postImg:
+			'https://w.namu.la/s/f103f7d2c60726a147027d0725bb52c6fe7466a830f46c9247f43fd47fbb546ba482f897704bd15b4b04d65e9fddced1451b59c2f5e300402435c557551eab6cff63fe1054862f2d55cda98152fb00218c30738fd6da6d5750ca7e3728b58be0',
+	},
+];
